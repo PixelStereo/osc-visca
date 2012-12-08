fd275689941ec473c7926fce255abcba23dd0cf0
Renaud Rubiano <contact@rubiano.fr>
2012-12-08 18:56:06 +0100

    …

--- osc_visca/osc_visca.ino
+++ osc_visca/osc_visca.ino
@@ -1,20 +1,319 @@
-//byte cam_pow_inq[] ={0x81, 0x01, 0x04, 0x07, 0x03, 0xFF, 0x0D};
-byte Address[] ={0x88, 0x30, 0x01, 0xFF};
+#include <Ethernet.h>
+#include <SPI.h>
+#include <ArdOSC.h>
+/************* Variables ****************/
+byte mac[] = { 0xDE, 0xAD, 0xBE, 0xEF, 0xFE, 0xED };
+byte ip[] = { 10, 0, 0, 10 };
+int serverPort = 10000;
+OSCServer server;
+EthernetClient client;
+
 void setup()
-{
+{ 
+    Ethernet.begin(mac, ip);
+  server.begin(serverPort);  
   Serial.begin(9600);
-  Serial.println("\r\n======Start======");
-  Serial.write(Address[0]);
-  Serial.write(Address[1]);
-  Serial.write(Address[2]);
-  Serial.write(Address[3]);
+  server.addCallback("/visca.1/sw",&ViscaSw);
+  server.addCallback("/visca.1/ir",&ViscaIR);
+  server.addCallback("/visca.1/backlight",&ViscaBackLight);
+  server.addCallback("/visca.1/reverse",&ViscaReverse);
+  server.addCallback("/visca.1/flip",&ViscaFlip);
+  server.addCallback("/visca.1/fx",&ViscaFX);
+  server.addCallback("/visca.1/freeze",&ViscaFreeze);
+  server.addCallback("/visca.1/whitebalance",&ViscaWB);
+  server.addCallback("/visca.1/mode",&ViscaExposure);
+  server.addCallback("/visca.1/gain",&ViscaGain);
+/*  server.addCallback("/visca.1/shutter",&ViscaShutter);
+  server.addCallback("/visca.1/iris",&ViscaIris);
+  server.addCallback("/visca.1/zoom/digital/sw",&ViscaDigitalSw);
+  server.addCallback("/visca.1/zoom/mode",&ViscaZoomMode);
+  server.addCallback("/visca.1/zoom",&ViscaZoom);
+  server.addCallback("/visca.1/focus/auto",&ViscaFocusAuto);
+  server.addCallback("/visca.1/focus/value",&ViscaFocusValue);
+  server.addCallback("/visca.1/focus/near",&ViscaFocusNear); */
 }
 
 void loop()
-{
-   if(Serial.available()){
-    char c = Serial.read();
-    Serial.print(c, HEX);
-    Serial.print(" ");
-  } 
+{ 
+    if(server.aviableCheck()>0)
+    {
+   
+    }
+}
+
+void ViscaSw(OSCMessage *_mes) {
+    int value = _mes->getArgInt32(0);
+      if ( value == 1 ) {
+           Serial.write(0x81);
+           Serial.write(0x01);
+           Serial.write(0x04);
+           Serial.write((uint8_t) 0);
+           Serial.write(0x02);
+           Serial.write(0xFF); 
+          } 
+      else { 
+          Serial.write(0x81);
+          Serial.write(0x01);
+          Serial.write(0x04);
+          Serial.write((uint8_t) 0);
+          Serial.write(0x03);
+          Serial.write(0xFF);
+          } 
+    }
+          
+void ViscaIR(OSCMessage *_mes) {
+    int value = _mes->getArgInt32(0);
+      if ( value == 1 ) {
+           Serial.write(0x81);
+           Serial.write(0x01);
+           Serial.write(0x04);
+           Serial.write(0x01);
+           Serial.write(0x02);
+           Serial.write(0xFF); 
+          } 
+      else { 
+          Serial.write(0x81);
+          Serial.write(0x01);
+          Serial.write(0x04);
+          Serial.write(0x01);
+          Serial.write(0x03);
+          Serial.write(0xFF);
+          } 
+}
+          
+void ViscaBackLight(OSCMessage *_mes) {
+    int value = _mes->getArgInt32(0);
+      if ( value == 1 ) {
+           Serial.write(0x81);
+           Serial.write(0x01);
+           Serial.write(0x04);
+           Serial.write(0x33);
+           Serial.write(0x02);
+           Serial.write(0xFF); 
+          } 
+      else { 
+          Serial.write(0x81);
+          Serial.write(0x01);
+          Serial.write(0x04);
+          Serial.write(0x33);
+          Serial.write(0x03);
+          Serial.write(0xFF);
+          } 
+}
+void ViscaFX(OSCMessage *_mes) {
+    int value = _mes->getArgInt32(0);
+      if ( value == 0 ) {
+          Serial.write(0x81);
+           Serial.write(0x01);
+           Serial.write(0x04);
+           Serial.write(0x63);
+           Serial.write((uint8_t) 0);
+           Serial.write(0xFF); 
+          } 
+      if ( value == 1 ) {
+          Serial.write(0x81);
+           Serial.write(0x01);
+           Serial.write(0x04);
+           Serial.write(0x63);
+           Serial.write(0x02);
+           Serial.write(0xFF); 
+          } 
+      if ( value == 2 ) {
+          Serial.write(0x81);
+           Serial.write(0x01);
+           Serial.write(0x04);
+           Serial.write(0x63);
+           Serial.write(0x04);
+           Serial.write(0xFF); 
+          } 
+}
+void ViscaReverse(OSCMessage *_mes) {
+    int value = _mes->getArgInt32(0);
+      if ( value == 1 ) {
+           Serial.write(0x81);
+           Serial.write(0x01);
+           Serial.write(0x04);
+           Serial.write(0x61);
+           Serial.write(0x02);
+           Serial.write(0xFF); 
+          } 
+      else { 
+          Serial.write(0x81);
+          Serial.write(0x01);
+          Serial.write(0x04);
+          Serial.write(0x61);
+          Serial.write(0x03);
+          Serial.write(0xFF);
+          } 
+}
+void ViscaFlip(OSCMessage *_mes) {
+    int value = _mes->getArgInt32(0);
+      if ( value == 1 ) {
+           Serial.write(0x81);
+           Serial.write(0x01);
+           Serial.write(0x04);
+           Serial.write(0x66);
+           Serial.write(0x02);
+           Serial.write(0xFF); 
+          } 
+      else { 
+          Serial.write(0x81);
+          Serial.write(0x01);
+          Serial.write(0x04);
+          Serial.write(0x66);
+          Serial.write(0x03);
+          Serial.write(0xFF);
+          } 
+}
+void ViscaFreeze(OSCMessage *_mes) {
+    int value = _mes->getArgInt32(0);
+      if ( value == 1 ) {
+           Serial.write(0x81);
+           Serial.write(0x01);
+           Serial.write(0x04);
+           Serial.write(0x62);
+           Serial.write(0x02);
+           Serial.write(0xFF); 
+          } 
+      else { 
+          Serial.write(0x81);
+          Serial.write(0x01);
+          Serial.write(0x04);
+          Serial.write(0x62);
+          Serial.write(0x03);
+          Serial.write(0xFF);
+          } 
+}
+void ViscaWB(OSCMessage *_mes) {
+    int value = _mes->getArgInt32(0);
+      if ( value == 0 ) {
+           Serial.write(0x81);
+           Serial.write(0x01);
+           Serial.write(0x04);
+           Serial.write(0x35);
+           Serial.write((uint8_t) 0);
+           Serial.write(0xFF); 
+          } 
+      if ( value == 1) {
+           Serial.write(0x81);
+           Serial.write(0x01);
+           Serial.write(0x04);
+           Serial.write(0x35);
+           Serial.write(0x01);
+           Serial.write(0xFF);
+          } 
+      if ( value == 2) {
+           Serial.write(0x81);
+           Serial.write(0x01);
+           Serial.write(0x04);
+           Serial.write(0x35);
+           Serial.write(0x02);
+           Serial.write(0xFF);
+          } 
+      if ( value == 3) {
+           Serial.write(0x81);
+           Serial.write(0x01);
+           Serial.write(0x04);
+           Serial.write(0x35);
+           Serial.write(0x03);
+           Serial.write(0xFF);
+          } 
+      if ( value == 4) {
+           Serial.write(0x81);
+           Serial.write(0x01);
+           Serial.write(0x04);
+           Serial.write(0x35);
+           Serial.write(0x04);
+           Serial.write(0xFF);
+          } 
+      if ( value == 5) {
+           Serial.write(0x81);
+           Serial.write(0x01);
+           Serial.write(0x04);
+           Serial.write(0x35);
+           Serial.write(0x05);
+           Serial.write(0xFF);
+          } 
+}
+void ViscaExposure(OSCMessage *_mes) {
+    int value = _mes->getArgInt32(0);
+      if ( value == 0 ) {
+           Serial.write(0x81);
+           Serial.write(0x01);
+           Serial.write(0x04);
+           Serial.write(0x39);
+           Serial.write((uint8_t) 0);
+           Serial.write(0xFF); 
+          } 
+      if ( value == 1) {
+           Serial.write(0x81);
+           Serial.write(0x01);
+           Serial.write(0x04);
+           Serial.write(0x39);
+           Serial.write(0x03);
+           Serial.write(0xFF);
+          } 
+      if ( value == 2) {
+           Serial.write(0x81);
+           Serial.write(0x01);
+           Serial.write(0x04);
+           Serial.write(0x39);
+           Serial.write(0x0A);
+           Serial.write(0xFF);
+          } 
+      if ( value == 3) {
+           Serial.write(0x81);
+           Serial.write(0x01);
+           Serial.write(0x04);
+           Serial.write(0x39);
+           Serial.write(0x0B);
+           Serial.write(0xFF);
+          } 
+}
+void ViscaGain(OSCMessage *_mes) {
+    int value = _mes->getArgInt32(0);
+      if ( value == 0 ) {
+           Serial.write(0x81);
+           Serial.write(0x01);
+           Serial.write(0x04);
+           Serial.write(0x4C);
+           Serial.write((uint8_t) 0);
+           Serial.write((uint8_t) 0);
+           Serial.write((uint8_t) 0);
+           Serial.write((uint8_t) 0);
+           Serial.write(0xFF); 
+          } 
+      if ( value == 1 ) {
+           Serial.write(0x81);
+           Serial.write(0x01);
+           Serial.write(0x04);
+           Serial.write(0x4C);
+           Serial.write((uint8_t) 0);
+           Serial.write((uint8_t) 0);
+           Serial.write((uint8_t) 0);
+           Serial.write(0x01);
+           Serial.write(0xFF); 
+          } 
+      if ( value == 2 ) {
+           Serial.write(0x81);
+           Serial.write(0x01);
+           Serial.write(0x04);
+           Serial.write(0x4C);
+           Serial.write((uint8_t) 0);
+           Serial.write((uint8_t) 0);
+           Serial.write((uint8_t) 0);
+           Serial.write(0x02);
+           Serial.write(0xFF); 
+          } 
+      if ( value == 3 ) {
+           Serial.write(0x81);
+           Serial.write(0x01);
+           Serial.write(0x04);
+           Serial.write(0x4C);
+           Serial.write((uint8_t) 0);
+           Serial.write((uint8_t) 0);
+           Serial.write((uint8_t) 0);
+           Serial.write(0x03);
+           Serial.write(0xFF); 
+          } 
 }
---
+++ test serial.maxpat
@@ -0,0 +1,214 @@
+{
+	"patcher" : 	{
+		"fileversion" : 1,
+		"appversion" : 		{
+			"major" : 6,
+			"minor" : 0,
+			"revision" : 7
+		}
+,
+		"rect" : [ 851.0, 275.0, 640.0, 480.0 ],
+		"bglocked" : 0,
+		"openinpresentation" : 0,
+		"default_fontsize" : 12.0,
+		"default_fontface" : 0,
+		"default_fontname" : "Arial",
+		"gridonopen" : 0,
+		"gridsize" : [ 15.0, 15.0 ],
+		"gridsnaponopen" : 0,
+		"statusbarvisible" : 2,
+		"toolbarvisible" : 1,
+		"boxanimatetime" : 200,
+		"imprint" : 0,
+		"enablehscroll" : 1,
+		"enablevscroll" : 1,
+		"devicewidth" : 0.0,
+		"description" : "",
+		"digest" : "",
+		"tags" : "",
+		"boxes" : [ 			{
+				"box" : 				{
+					"fontname" : "Arial",
+					"fontsize" : 12.0,
+					"id" : "obj-5",
+					"maxclass" : "message",
+					"numinlets" : 2,
+					"numoutlets" : 1,
+					"outlettype" : [ "" ],
+					"patching_rect" : [ 261.0, 279.0, 104.0, 18.0 ],
+					"presentation_rect" : [ 261.0, 279.0, 0.0, 0.0 ],
+					"text" : "/serial.2 65 84 13"
+				}
+
+			}
+, 			{
+				"box" : 				{
+					"fontname" : "Arial",
+					"fontsize" : 12.0,
+					"id" : "obj-7",
+					"maxclass" : "message",
+					"numinlets" : 2,
+					"numoutlets" : 1,
+					"outlettype" : [ "" ],
+					"patching_rect" : [ 245.0, 256.0, 120.0, 18.0 ],
+					"presentation_rect" : [ 245.0, 256.0, 0.0, 0.0 ],
+					"text" : "/serial.1 22 44 33"
+				}
+
+			}
+, 			{
+				"box" : 				{
+					"fontname" : "Arial",
+					"fontsize" : 12.0,
+					"id" : "obj-2",
+					"maxclass" : "message",
+					"numinlets" : 2,
+					"numoutlets" : 1,
+					"outlettype" : [ "" ],
+					"patching_rect" : [ 118.0, 148.0, 104.0, 18.0 ],
+					"presentation_rect" : [ 122.0, 150.0, 0.0, 0.0 ],
+					"text" : "/serial.2 65 84 13"
+				}
+
+			}
+, 			{
+				"box" : 				{
+					"fontname" : "Arial",
+					"fontsize" : 13.0,
+					"id" : "obj-32",
+					"maxclass" : "message",
+					"numinlets" : 2,
+					"numoutlets" : 1,
+					"outlettype" : [ "" ],
+					"patching_rect" : [ 27.0, 199.0, 61.0, 19.0 ],
+					"text" : "65 84 13"
+				}
+
+			}
+, 			{
+				"box" : 				{
+					"id" : "obj-8",
+					"maxclass" : "toggle",
+					"numinlets" : 1,
+					"numoutlets" : 1,
+					"outlettype" : [ "int" ],
+					"parameter_enable" : 0,
+					"patching_rect" : [ 106.0, 94.0, 20.0, 20.0 ]
+				}
+
+			}
+, 			{
+				"box" : 				{
+					"fontname" : "Arial",
+					"fontsize" : 12.0,
+					"id" : "obj-6",
+					"maxclass" : "message",
+					"numinlets" : 2,
+					"numoutlets" : 1,
+					"outlettype" : [ "" ],
+					"patching_rect" : [ 102.0, 125.0, 104.0, 18.0 ],
+					"text" : "/serial.1 65 84 13"
+				}
+
+			}
+, 			{
+				"box" : 				{
+					"fontname" : "Arial",
+					"fontsize" : 12.0,
+					"id" : "obj-4",
+					"maxclass" : "newobj",
+					"numinlets" : 1,
+					"numoutlets" : 0,
+					"patching_rect" : [ 102.0, 209.0, 145.0, 20.0 ],
+					"text" : "udpsend localhost 44000"
+				}
+
+			}
+, 			{
+				"box" : 				{
+					"fontname" : "Arial",
+					"fontsize" : 12.0,
+					"id" : "obj-3",
+					"maxclass" : "message",
+					"numinlets" : 2,
+					"numoutlets" : 1,
+					"outlettype" : [ "" ],
+					"patching_rect" : [ 313.0, 117.0, 50.0, 18.0 ],
+					"text" : "print"
+				}
+
+			}
+, 			{
+				"box" : 				{
+					"fontname" : "Arial",
+					"fontsize" : 12.0,
+					"id" : "obj-1",
+					"maxclass" : "newobj",
+					"numinlets" : 1,
+					"numoutlets" : 2,
+					"outlettype" : [ "int", "" ],
+					"patching_rect" : [ 294.0, 166.0, 39.0, 20.0 ],
+					"text" : "serial"
+				}
+
+			}
+ ],
+		"lines" : [ 			{
+				"patchline" : 				{
+					"destination" : [ "obj-4", 0 ],
+					"disabled" : 0,
+					"hidden" : 0,
+					"source" : [ "obj-2", 0 ]
+				}
+
+			}
+, 			{
+				"patchline" : 				{
+					"destination" : [ "obj-4", 0 ],
+					"disabled" : 0,
+					"hidden" : 0,
+					"source" : [ "obj-32", 0 ]
+				}
+
+			}
+, 			{
+				"patchline" : 				{
+					"destination" : [ "obj-4", 0 ],
+					"disabled" : 0,
+					"hidden" : 0,
+					"source" : [ "obj-5", 0 ]
+				}
+
+			}
+, 			{
+				"patchline" : 				{
+					"destination" : [ "obj-4", 0 ],
+					"disabled" : 0,
+					"hidden" : 0,
+					"source" : [ "obj-6", 0 ]
+				}
+
+			}
+, 			{
+				"patchline" : 				{
+					"destination" : [ "obj-4", 0 ],
+					"disabled" : 0,
+					"hidden" : 0,
+					"source" : [ "obj-7", 0 ]
+				}
+
+			}
+, 			{
+				"patchline" : 				{
+					"destination" : [ "obj-6", 0 ],
+					"disabled" : 0,
+					"hidden" : 0,
+					"source" : [ "obj-8", 0 ]
+				}
+
+			}
+ ],
+		"dependency_cache" : [  ]
+	}
+
+}
