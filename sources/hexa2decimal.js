cc01542ccc3bd880bdd50d9db2e3e44d4fd60ffc
Renaud Rubiano <contact@rubiano.fr>
2013-06-18 21:19:47 +0200

    add a single javascript for hex a to decimal conversion

---
+++ sources/hexa2decimal.js
@@ -0,0 +1,13 @@
+	
+/*
+	convert hexa to decimal
+*/
+
+outlets = 1;
+setinletassist(0,"single hexa to convert");
+setoutletassist(0,"single decimal converted");
+
+function hexa(h) {
+	outlet(0, parseInt(h,16))
+}
+	
